1ee11365883f95b5dd76d49fad6d9b4b25f5fea1
shri <49117414+shrishtisingh12345@users.noreply.github.com>
2021-06-20 22:39:09 +0530

    Add files via upload

---
+++ music play/music play/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  music play
+//
+//  Created by shrishti singh on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func anymu(_ sender: UIButton) {
+        print("uff")
+    }
+    @IBAction func butt(_ sender: Any) {
+        print("uff")
+    }
+    
+    
+}
+
